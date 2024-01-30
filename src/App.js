e5f72851e41cfa7619a61acebb3e670ae06b83dd
Gosula Sirisha <sirishagosula@gmail.com>
2024-01-30 19:22:40 +0530

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import About from "./pages/Home/Section1"; // Adjust imports as needed
+import OurMenu from "./pages/Home/Section2";
+import Shop from "./pages/Home/Section4";
+import Blog from "./pages/Home/Section5";
+import Contact from "./pages/Home/Section7";
+ // Create a Cart component if not already present
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/menu" element={<OurMenu />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+        
+      </Routes>
+    </Router>
   );
 }
 
